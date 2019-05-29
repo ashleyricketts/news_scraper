9a5b574ffe0286314eea27c3367c2e740e5d16e3
Ashley Ricketts <aricketts12@gmail.com>
2019-05-29 15:16:51 -0500

    code for pushing scraped data to mongo db

--- server.js
+++ server.js
@@ -1,7 +1,45 @@
+var express = require("express");
+var mongojs = require("mongojs");
 var cheerio = require("cheerio");
 var axios = require("axios");
 
-axios.get("https://www.aljazeera.com/topics/regions/us-canada.html").then(function(response) {
+// intialize express
+var app = express();
+
+// database configuration 
+var databaseUrl = "news_scraper";
+var collections = ["scrapedNews"];
+
+// hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+// main route (simple Hello World Message)
+app.get("/", function(req, res) {
+    res.send("Hello world");
+  });
+
+
+// Retrieve data from the db
+app.get("/all", function(req, res) {
+    // Find all results from the scrapedData collection in the db
+    db.scrapedNews.find({}, function(error, found) {
+      // Throw any errors to the console
+      if (error) {
+        console.log(error);
+      }
+      // If there are no errors, send the data to the browser as json
+      else {
+        res.json(found);
+      }
+    });
+  });
+
+// scrape data from one site and place it into the mongodb db
+app.get("/scrape", function(req, res){
+    axios.get("https://www.aljazeera.com/topics/regions/us-canada.html").then(function(response) {
 
     var $ = cheerio.load(response.data);
 
@@ -11,12 +49,31 @@ axios.get("https://www.aljazeera.com/topics/regions/us-canada.html").then(functi
 
         var title = $(element).parent().text();
         var link = $(element).parent().parent().find("a").attr("href");
-        
-        results.push({
-            title: title,
-            link: link
-          });
-    });
+        // var summary = $(element).parent().find("p");
 
-    console.log(results);
+        if(title && link) {
+            db.scrapedNews.insert({
+                title: title,
+                link: link
+            },
+            function(err, inserted){
+                if (err){
+                    console.log(err);
+                }
+                else{
+                    console.log(inserted);
+                }
+            });
+        }
+
+    });
+    });
+    res.send("Scrape Complete");
 });
+
+//listen on port 8080
+app.listen(8080, function() {
+    console.log("App running on port 8080!")
+})
+
+
