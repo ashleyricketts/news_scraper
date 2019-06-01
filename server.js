822715cb974ba8133f6ff599399e181f5e1049c5
Ashley Ricketts <aricketts12@gmail.com>
2019-06-01 17:20:26 -0500

    route for scraping

--- server.js
+++ server.js
@@ -1,23 +1,59 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// scraping tools
 var cheerio = require("cheerio");
 var axios = require("axios");
 
+var db = require("./models");
 
-axios.get("https://www.npr.org/sections/news/")
+var PORT = 8080;
+
+// initialize express
+var app = express();
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/NPRscraper", { useNewUrlParser: true });
+
+
+// Routes
+
+// get route to scrape NPR website
+app.get("/scrape", function(req, res) {
+
+    axios.get("https://www.npr.org/sections/news/")
     .then(function(response) {
         
         var $ = cheerio.load(response.data);
 
-        var results = [];
-
         $("div.item-info").each(function(i, element) {
 
-            var title = $(element).children("h2").text();
+            var result= {};
+            result.title = $(this).children("h2").text();
             // console.log("+" + title);
-
-            var summary = $(element).children("p.teaser").text();
+            result.summary = $(element).children("p.teaser").text();
             // console.log("&" + summary);
-
-            var link = $(element).children("h2").children("a").attr("href");
+            result.link = $(element).children("h2").children("a").attr("href");
             // console.log("*" + link);
-        });
+            // console.log(result);
+
+        }); 
+    
+        res.send("Scrape Complete");
     });
+});
+
+
+
+
+
+
